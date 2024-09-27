e9593dcc08adc9b6d4f4f32569632f224202cf87
anna <anka-kudrow@mail.ru>
2024-09-27 13:53:27 +0300

    added achievments

--- src/components/Main/Achievments/Achievments.tsx
+++ src/components/Main/Achievments/Achievments.tsx
@@ -7,18 +7,154 @@ export const Achievments = () => {
               <h2 className="achievements__title title">Достижения</h2>
               <ul className="achievements__list">
                 <li className="achievements__item">
-                  <span className="achievements__year" >2001Г.</span><br/>
-                  Участие в конкурсе "Московская краса".
+                  <span className="achievements__year" >2012 - 2017Г.</span><br/>
+                  Церемония открытия и стартов ралли «Шелковый путь» на Красной площади.
                 </li>
                 <li className="achievements__item">
-                  <span className="achievements__year" >2002Г.</span><br/>
-                  Участие в конкурсе "".
+                  <span className="achievements__year" >2014 - 2017Г.</span><br/>
+                  Торжественная церемония вручения дипломов выпускникам МГМСУ.
                 </li>
                 <li className="achievements__item">
-                  <span className="achievements__year" >2003Г.</span><br/>
-                  Участие в fashion super show.
+                  <span className="achievements__year" >2017 - 2018Г.</span><br/>
+                  Церемония открытия фестиваля «Россия любит футбол» в Олимпийском комплексе «Лужники», Москва.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2016Г.</span><br/>
+                  Церемония открытия чемпионата по гребле на байдарках, каноэ и параканоэ, Гребной канал «Крылатское».
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2017 - 2019Г.</span><br/>
+                  Финал смотра-конкурса среди должностных лиц федеральной таможенной службы.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2017 - 2022Г.</span><br/>
+                  Торжественная церемония закрытия Международных Армейских Игр.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2018Г.</span><br/>
+                  Торжественная церемония открытия Чемпионата Европы по фигурному катанию.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2019Г.</span><br/>
+                  Открытие чемпионата Европы по гребле на лодках «Дракон» среди национальных и клубных команд.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2019Г.</span><br/>
+                  Мероприятие 210 лет Министерству Транспорта в Кремлевском дворце, Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2019, 2021Г.</span><br/>
+                  Елка Мэра в Гостином дворе, Москва.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2019 - 2021Г.</span><br/>
+                  Праздничный концерт ко Дню защитника Отечества в Кремлевском дворце, Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2020, 2021Г.</span><br/>
+                  Праздничный концерт ко Дню сотрудника органов внутренних дел РФ.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2021Г.</span><br/>
+                  Работа в Государственном Академическом Большом Театре, Москва.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2022Г.</span><br/>
+                  Отчётный концерт Детского музыкального театра «Домисольки», Москва.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2022Г.</span><br/>
+                  Концерт ко дню города МузТВ в парке Победы, Москва.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2022Г.</span><br/>
+                  Концерт Домисолька «Новогодняя ночь в школе», Кремль, Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2022Г.</span><br/>
+                  Концерт театра российской армии.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2022Г.</span><br/>
+                  Съемки новогодней программы г. Донецк. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2023Г.</span><br/>
+                  Съемки концерта для оперной певицы Ксении Дежневой в Кремле, Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2023, 2024Г.</span><br/>
+                  Концерт «23 февраля» на 1 канале в прямом эфире в театре «Российской армии», Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2023Г.</span><br/>
+                  Съемки концерта к 8 марта  для канала «Звезда» в театре «Российской армии», Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2023Г.</span><br/>
+                  Съемки концерта посвященного столетию ЦСКА для канала «Звезда» в театре «Российской армии», Москва. 
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2023Г.</span><br/>
+                Концерт в ЦДКЖ.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2024Г.</span><br/>
+                    Главный гример “welcome zone” на «Ёлке Мэра в гостином дворе», Москва.
+                </li>
+                <li className="achievements__item">
+                  <span className="achievements__year" >2024Г.</span><br/>
+                  Мастер класс «Макияж для себя» посвященный международному женскому дню на стенде Самара на выставке «Россия» в ВДНХ, Москва. 
                 </li>
               </ul>
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
           </section>
   )
