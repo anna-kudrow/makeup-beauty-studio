1b4025adba88646d4de3d33b47732b9be40a60b8
anna <anka-kudrow@mail.ru>
2024-10-02 11:21:01 +0300

    add git ignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/boyarinova-studio/public_html/',
+  // base: '/boyarinova-studio/public_html/',
 })
 
 // /makeup-beauty-studio/
