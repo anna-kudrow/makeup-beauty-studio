0ee0626d58686f8416895838f2bebfc64abdabae
anna <anka-kudrow@mail.ru>
2024-10-06 18:04:23 +0300

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/boyarinova-studio/public_html/',
+  base: '/boyarinova-studio/'
 })
 
