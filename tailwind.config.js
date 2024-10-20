c77b5c78e3c2523de5237c6c3feb4c7205c277de
anna <anka-kudrow@mail.ru>
2024-10-20 15:26:18 +0300

    shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,69 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primaryBackground: "var(--primary-background)",
-        mainText: "var(--main-text)",
-        headText: "var(--head-text)",
-        rouge: "var(--rouge)",
-        black: "var(--black)",
-        purple: "var(--purple)",
-        purpleText: "var(--purple-text)",
-        lightPurple: "var(--light-purple)",
-        white: "var(--white)",
-        softPink: "var(--soft-pink)",
-        lightButtonBg: "var(--light-button-bg)",
-        lightBrown: "var(--light-brown)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primaryBackground: 'var(--primary-background)',
+  			mainText: 'var(--main-text)',
+  			headText: 'var(--head-text)',
+  			rouge: 'var(--rouge)',
+  			black: 'var(--black)',
+  			purple: 'var(--purple)',
+  			purpleText: 'var(--purple-text)',
+  			lightPurple: 'var(--light-purple)',
+  			white: 'var(--white)',
+  			softPink: 'var(--soft-pink)',
+  			lightButtonBg: 'var(--light-button-bg)',
+  			lightBrown: 'var(--light-brown)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
