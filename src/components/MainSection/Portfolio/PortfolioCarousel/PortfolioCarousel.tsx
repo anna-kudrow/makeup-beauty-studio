014d4f8f6408576da681537d1e188d4cc373e995
anna <anka-kudrow@mail.ru>
2024-10-22 12:37:39 +0300

    add carousel for portfolio

--- src/components/MainSection/Portfolio/PortfolioCarousel/PortfolioCarousel.tsx
+++ src/components/MainSection/Portfolio/PortfolioCarousel/PortfolioCarousel.tsx
@@ -2,38 +2,58 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel"
+
+import { type CarouselApi } from "@/components/ui/carousel"
+import { useEffect, useState } from 'react';
 
 
 const backstagePhotoData = [
   {
-    src: '/images/backstage/agutin.jpg',
+    src: '/images/portfolio/backstage/agutin.jpg',
     alt: 'фото с Агутиным'
   },
   {
-    src: '/images/backstage/army.jpg',
+    src: '/images/portfolio/backstage/army.jpg',
     alt: 'фото с концерта Российской армии'
   },
   {
-    src: '/images/backstage/boyarskii.jpg',
+    src: '/images/portfolio/backstage/boyarskii.jpg',
     alt: 'фото с Боярским'
   },
   {
-    src: '/images/backstage/domisolka.jpg',
+    src: '/images/portfolio/backstage/domisolka.jpg',
     alt: 'фото с концерта Домисолька'
   },
 ];
 
 export const PortfolioCarousel = () => {
+
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0)
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+    setCurrent(api.selectedScrollSnap())
+ 
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap())
+    })
+  }, [api])
+
   return (
-        <Carousel>
+        <Carousel className='p-2' setApi={setApi} opts={{loop: true}}>
+          <div className='flex justify-between'>
+            <h2>Backstage</h2>
+            <div className='flex gap-3'>
+              <button onClick={()=> api?.scrollTo(current - 1)}><img src="/images/portfolio/gallery-left.svg" alt="перелистнуть влево" /></button>
+              <button onClick={()=> api?.scrollTo(current + 1)}><img src="/images/portfolio/gallery-right.svg" alt="перелистнуть вправо" /></button>
+            </div>
+          </div>
           <CarouselContent>
-            {backstagePhotoData.map(photo => (<CarouselItem><img src={photo.src} alt={photo.alt} /></CarouselItem>))}
+            {backstagePhotoData.map(photo => (<CarouselItem className='carousel-item' key={photo.alt}><img src={photo.src} alt={photo.alt} /></CarouselItem>))}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
         </Carousel>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
 
 
 createRoot(document.getElementById("root")!).render(
