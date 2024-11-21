cbc5fc19ee2f0e48382c2908bcf9e100ea6076d0
anna <anka-kudrow@mail.ru>
2024-11-21 23:55:10 +0300

    new method for 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/404.html',
+          dest: './',
+        },
+      ],
+    }),
+  ],
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
